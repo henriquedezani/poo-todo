573be6e3dea47567f027d6109cb42113584a5c80
Henrique Dezani <dezani@gmail.com>
2021-09-03 17:22:21 -0300

    Exemplo de nome de arquivo CSHTML diferente do nome do método

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,18 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TodoList.Controllers
 {
+    
     public class TodoController : Controller
     {
+        // https://localhost:5001/todo/index
+        // https://localhost:5001/[NOME DO CONTROLLER (SEM O TEXTO "CONTROLLER")]/[NOME DO MÉTODO]
         public ActionResult Index()
         {
             // retornará o HTML definido no arquivo Index da pasta Views/Todo
+            // return Views/Todo/Index.cshtml
             return View();
         }
 
-        // http://localhost/todo/hello
+        // https://localhost:5001/todo/hello
         public ActionResult Hello()
         {
-            return View();
+            // Views/Todo/Oi.cshtml
+            return View("Oi");
         }
     }
 }
