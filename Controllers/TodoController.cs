7358a8dfa47cfdc53c36a64230a363894e7f980a
Henrique Dezani <dezani@gmail.com>
2021-09-14 18:34:12 -0300

    Refatora o Create para usar DataBinding

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -8,34 +8,12 @@ namespace TodoList.Controllers
 {    
     public class TodoController : Controller
     {
+        private static List<Tarefa> tarefas = new List<Tarefa>();
+
         // http://localhost/Todo/Index = return new TodoController().Index()
         public ActionResult Index()
         {
-            List<Tarefa> tarefas = new List<Tarefa>();
-
-            Tarefa t1 = new Tarefa();
-            t1.Id = 1;
-            t1.Texto = "Passear com o cachorro";
-            t1.Concluida = false;
-
-            Tarefa t2 = new Tarefa();
-            t2.Id = 2;
-            t2.Texto = "Lavar o carro";
-            t2.Concluida = true;
-
-            Tarefa t3 = new Tarefa();
-            t3.Id = 3;
-            t3.Texto = "Estudar para prova";
-            t3.Concluida = true;
-
-            tarefas.Add(t1);
-            tarefas.Add(t2);
-            tarefas.Add(t3);
-
             return View(tarefas);
-            // return View();
-            // return View("Index");
-            // return View("Index", tarefas);
         }
 
         // http://localhost:5000/todo/create
@@ -47,10 +25,10 @@ namespace TodoList.Controllers
 
         // http://localhost:5000/todo/create
         [HttpPost] // <form method="POST" ...
-        public ActionResult Create(IFormCollection form)
+        public ActionResult Create(Tarefa tarefa)
         {
-            Console.WriteLine(form["texto"]); // <input name="texto"..
-            return View();
+            tarefas.Add(tarefa);
+            return RedirectToAction("Index"); // HTTP 300
         }
     }
 }
