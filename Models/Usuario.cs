fcad87e8a0df13e9a373fae6ebf503a04044308f
Henrique Dezani <dezani@gmail.com>
2021-10-01 18:25:08 -0300

    Inicia criação do Usuario (model)

---
+++ Models/Usuario.cs
@@ -0,0 +1,18 @@
+namespace TodoList.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
+
+// CREATE TABLE Usuario
+// (
+//     Id          int             primary KEY     identity,
+//     Nome        varchar(100)    not null,
+//     Email       varchar(100)    not null        unique,
+//     Senha       varchar(100)    not null
+// )
